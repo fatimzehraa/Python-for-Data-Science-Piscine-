5d7b9a31774321078798f231e73d1547cdd111b0
fatimzehraa <elbouaazzaouitema@gmail.com>
2025-01-05 20:46:33 +0100

    ex09 && venv in root

---
+++ python_0/ex09/my-package/src/my_package/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present fatimzehraa <elbouaazzaouitema@gmail.com>
+#
+# SPDX-License-Identifier: MIT
---
+++ python_0/ex09/my-package/src/my_package/my_package.py
@@ -0,0 +1,3 @@
+class my_package:
+    def __init__(self) -> None:
+        pass
